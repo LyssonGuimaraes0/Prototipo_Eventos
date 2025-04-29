a9589ec7761ce0a5d44b881399f88ece1c955532
Your Name <guilhermesmt3987@gmail.com>
2025-04-29 15:34:48 -0300

    Fix: Refatorar a estrutura do projeto: remover trechos não utilizados e implementar novas seções de navegação e banner

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<?php  include('./snippets/header.html');?>
+<body>
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/navegacao.html'); ?>
+
+    <!-- Sessão Banner -->
+    <?php include('./snippets/banner.html'); ?>
+
+    <!-- Noticias -->
+    <?php include('./snippets/noticia.html'); ?>
+
+    <!-- Sessão Sobre -->
+    <?php include('./snippets/sobre.html'); ?>
+
+    <!-- Sessão Termo -->
+    <?php include('./snippets/termo.html'); ?>
+
+    <!-- Sessão de Download -->
+    <?php include('./snippets/download.html'); ?>
+
+    <!-- Sessão dos Patrocinadores -->
+    <?php include('./snippets/patrocinio.html'); ?>
+
+    <!-- Botão flutuante compartilhar -->
+    <?php include('./snippets/botao-flutuante.html'); ?>
+
+    <!-- Footer -->
+    <?php include('./snippets/rodape.html'); ?>
+
+    <!-- Chamada dos Scripts -->
+    <?php include('./snippets/scripts.html'); ?>
+</body>
+</html>
