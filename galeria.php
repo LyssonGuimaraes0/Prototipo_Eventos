aaaa39b4d551d32df8e42b519a631bb4b9b9ade0
AlanQS2 <alanqueiroz00@hotmail.com>
2025-07-17 15:36:26 -0300

    Feat: Add gallery section with responsive design and navigation

---
+++ galeria.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    
+<head>
+<?php  include('./snippets/header.html');?>
+</head>
+
+<body>
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/navegacao-galeria.html'); ?>
+    <!-- Sessão da Galeria -->
+    <?php include('./snippets/galeria.html'); ?>
+
+     <!-- Sessão dos Patrocinadores -->
+    <?php include('./snippets/patrocinadores.html'); ?>
+
+    <!-- Footer -->
+    <?php include('./snippets/rodape.html'); ?>
+
+    <!-- Chamada dos Scripts -->
+    <?php include('./snippets/scripts.html'); ?>
+</body>
+</html>
